3fa0dadbe5fdc29029d9f72d1c0a64cdbb8f92c0
SharneSun <ssun2@caltech.edu>
2019-08-12 17:52:49 -0600

    Finished up ESRNN Model

--- src/kpforecast/ml/esrnn/ESRNN_model.py
+++ src/kpforecast/ml/esrnn/ESRNN_model.py
@@ -9,6 +9,7 @@ class ESRNN_model(nn.Module):
     def __init__(self, num_series, configuration):
         self.configuration = configuration
         self.num_series = num_series
+        self.add_nl_layer = configuration['add_nl_layer']
 
 
         # Setting up per series parameters, alpha, gamma, and seasonality
@@ -23,7 +24,7 @@ class ESRNN_model(nn.Module):
         self.scoring = nn.Linear(configuration['state_hsize'], configuration['state_hsize'])
         self.DRNN = RESIDUALDRNN(configuration)
                 
-    def forward(self, train, val, test, info_cat, idxs):
+    def forward(self, train, val, test, info_cat, idxs, testing):
         # Obtaining the per series parameters for the batch that we are training on
         alphas = self.logistic(torch.stack([self.level_smoothing_coef[idx] for idx in idxs]).squeeze(1))
         gammas = self.logistic(torch.stack([self.season_smoothing_coef[idx] for idx in idxs]).squeeze(1))
@@ -33,6 +34,9 @@ class ESRNN_model(nn.Module):
         # computing the i + 1 seasonality term for all series at once
         seasonalities = torch.transpose(torch.stack(seasonalities, seasonalities[0]), 0, 1)
         
+        if testing:
+            train = torch.cat((train, val), dim=1)
+
         levels = [train[:, 0] / seasonalities[0]]
         log_diff_of_levels = []
 
@@ -43,14 +47,75 @@ class ESRNN_model(nn.Module):
             log_diff_of_levels.append(torch.log(levels[i] / levels[i - 1]))
             
             # Calculating seasonalities per series
-            seasonalities.append(gammas * (train[:, i] / levels[i]) + (1 - gammas) * seasonalities[i])
+            seasonalities = torch.stack(seasonalities, gammas * (train[:, i] / levels[i]) + (1 - gammas) * seasonalities[i])
 
-        stacked_seasonalities = torch.transpose(seasonalities, 0, 1)
+        seasonalities = torch.transpose(seasonalities, 0, 1)
         stacked_levels = torch.transpose(levels, 0, 1)
 
-        log_mean_sq_log_diff_level = 0
+        loss_mean_sq_log_diff_level = 0
         if self.configuration['level_variability_penalty'] > 0:
-            sq_log_diff = 
+            sq_log_diff = torch.stack([(log_diff_of_levels[i] - log_diff_of_levels[i - 1]) ** 2 for i in range(1, len(log_diff_of_levels))])
+            loss_mean_sq_log_diff_level = torch.mean(sq_log_diff)
+        
+        if self.configuration['output_size'] > self.configuration['seasonality']:
+            start_extension = seasonalities.shape[1] - self.configuration['seasonality']
+            seasonalities = torch.stack(seasonalities, seasonalities[:, :start_extension], dim = 1)
+
+
+        input_list = []
+        output_list = []
+
+        for i in range(self.configuration['input_size'] - 1, train.shape[1]):
+            input_start = i + 1 - self.configuration['input_size']
+            input_end = i + 1
+
+            deseasonalized_train_input = train[:, input_start : input_end] / seasonalities[:, input_start : input_end]
+            deseasonalized_norm_train_input = deseasonalized_train_input / stacked_levels[:, i].unsqueeze(1)
+            deseasonalized_norm_train_input_with_info = torch.cat((deseasonalized_norm_train_input, info_cat), dim=1)
+
+            input_list.append(deseasonalized_norm_train_input_with_info)
+
+            output_start = i + 1
+            output_end = i + 1 + self.configuration['output_size'] 
+
+            if i < train.shape[1] - self.configuration['output_size']:
+                deseasonalized_train_output = train[:, output_start : output_end] / seasonalities[:, output_start : output_end]
+                deseasonalized_norm_train_output = deseasonalized_train_output / stacked_levels[:, i].unsqueeze(1)
+                input_list.append(deseasonalized_norm_train_output)
+            
+        input_list = torch.cat([i.unsqueeze(0) for i in input_list])
+        output_list = torch.cat([i.unsqueeze(0) for i in output_list])
+
+        self.train()
+
+        predictions = self.series_forward(input_list[ : -self.configuration['output_size']])
+        network_act = output_list
+
+        self.eval()
+        
+        output_non_train = self.series_forward(window_input)
+
+        # USE THE LAST VALUE OF THE NETWORK OUTPUT TO COMPUTE THE HOLDOUT PREDICTIONS
+        hold_out_reseas = output_non_train[-1] * seasonalities[:, -self.configuration['output_size'] : ]
+        hold_out_renorm = hold_out_reseas * stacked_levels[:, -1].unsqueeze(1)
+
+        hold_out_pred = hold_out_renorm * torch.gt(hold_out_renorm, 0).float()
+        hold_out_act = test if testing else val
+
+        hold_out_act_deseas = hold_out_act.float() / seasonalities[:, -self.configuration['output_size']:]
+        hold_out_act_deseas_norm = hold_out_act_deseas / stacked_levels[:, -1].unsqueeze(1)
+
+        self.train()
+
+        return predictions, network_act, (hold_out_pred, output_non_train), (hold_out_act, hold_out_act_deseas_norm), loss_mean_sq_log_diff_level
+    
+    def series_forward(self, data):
+        data = self.DRNN(data)
+        if self.add_nl_layer():
+            data = self.nl_layer(data)
+            data = self.act(data)
+        data = self.scoring(data)
+        return data
 
 
 class RESIDUALDRNN(nn.Module):
--- src/kpforecast/ml/esrnn/TrainESRNN.py
+++ src/kpforecast/ml/esrnn/TrainESRNN.py
@@ -3,9 +3,33 @@ import torch.nn as torch.nn
 
 
 class TrainESRNN(nn.Module):
-    def __init__(self):
+    def __init__(self, model, dataloader, run_id, config, ohe_headers):
         # initalize TrainESRNN
+        super(TrainESRNN, self).__init__()
+        self.model = model.to(configuration['device'])
+        self.configuration = configuration
+        self.dl = dataloader
+        self.ohe_headers = ohe_headers
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=configuration['learning_rate'])
+
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, 
+                        step_size=configuration['lr_anneal_step'], gamma=conifguration['lr_anneal_rate'])
+        
+        self.criterion = PinballLoss(self.configuration['training_tau', 
+                        self.configuration['output_size'] * self.configuration['batch_size'], self.configuration['device']])\
+        
+        self.epochs = 0
+        self.max_epochs = configuration['epochs']
+        self.run_id = run_id
+        self.prod_string = 'prod' if configuration['prod'] else 'dev'
+        self.log = Logger("../logs/train%s%s%s" % (self.config['variable'], self.prod_str, self.run_id))
+        self.csv_save_path = None
+
+    def train_epochs(self):
+        max_loss = 1e8
+        
 
     def train(self):
 
-    def train_batch(self, train, val, test, info_cat, idx)
+    def train_batch(self, train, val, test, info_cat, idx):
+
